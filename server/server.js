4e8d0ecff79add85563569191c86ef2e36b277b1
yoanybejarano <yoanybejarano@gmail.com>
2018-09-09 17:07:30 -0500

    Get all todos and users

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -23,8 +23,31 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find({}).then((todos) => {
+        res.send({
+            'count': todos.length,
+            todos
+        });
+    }, (err) => {
+        console.log(err);
+    });
+});
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(
+            {
+                'count': users.length,
+                users
+            });
+    }, (err) => {
+        console.log(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports = { app };
+module.exports = {app};
