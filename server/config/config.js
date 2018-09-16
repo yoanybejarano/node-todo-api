2a1041b5f4468c627eb693286d0711bf6bc49172
Unknown <yoanybejarano@gmail.com>
2018-09-16 10:39:43 -0500

    setting enviroment

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 
-console.log('env *****', env);
+if (env === 'test' || env === 'development') {
+    var config = require('./config.json');
+    var envConfig = config[env];
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/todo-app';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/todo-app-test';
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
+
 
 
 
